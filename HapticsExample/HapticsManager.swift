bc623d67ab197d96f29bb1183fa582bcea33c415
27293 <myltd6@fpt.com.vn>
2020-12-21 11:25:51 +0700

    fix bug

--- HapticsExample/HapticsManager.swift
+++ HapticsExample/HapticsManager.swift
@@ -4,5 +4,27 @@
 //
 //  Created by Ngo Dang tan on 21/12/2020.
 //
+import UIKit
 
-import Foundation
+final class HapticsManager {
+    static let shared = HapticsManager()
+    
+    private init(){}
+    
+    public func selectionVibrate(){
+        DispatchQueue.main.async {
+            let selectionFeedbackGenerator = UISelectionFeedbackGenerator()
+            selectionFeedbackGenerator.prepare()
+            selectionFeedbackGenerator.selectionChanged()
+        }
+ 
+    }
+    public func vibrate(for type: UINotificationFeedbackGenerator.FeedbackType){
+        DispatchQueue.main.async {
+            let notificationGenerator = UINotificationFeedbackGenerator()
+            notificationGenerator.prepare()
+            notificationGenerator.notificationOccurred(type)
+        }
+    
+    }
+}
--- HapticsExample/ViewController.swift
+++ HapticsExample/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @objc private func successfullyBookedFlight(){
+        HapticsManager.shared.vibrate(for: .success)
+        
+    }
 
 }
 
